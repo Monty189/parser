95eb40db80b36bd41d354ee30e93a50e7e8e81ec
Monty189 <ao.kamaev89@gmail.com>
2024-11-29 18:20:25 +0000

    -

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://tl.rulate.ru/book/16292/327323/ready_new"
+    url = "https://mebel-v-koroleve.ru/stulya_i_taburetyi"
     response = requests.get(url)
     print(response.status_code)
+    #print(response.text)
+
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    part = soup.find('div', class_='text-container')
+    spoon = soup.find("div", class_="products-list__body")
+    print([i.text for i in spoon])
+
     with open('page.txt', 'a', encoding='UTF-8') as file:
-        file.write(part.text)
+        file.write(''.join([i.text for i in spoon]))
+
+    #soup = BeautifulSoup(response.text, 'html.parser') 
+    #part = soup.find('div', class_='text-container')
+    #with open('page.txt', 'a', encoding='UTF-8') as file:
+        #file.write(part.text)
 
 #    with open('output.html', 'a', encoding='UTF-8') as file:
 #        file.write(response.text)
